0274ad25f3d03b661db4557cd9bc29d3cd3a41e4
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-30 23:45:51 +0100

    active link worked on

--- Client/src/components/MainNav/MainNav.jsx
+++ Client/src/components/MainNav/MainNav.jsx
@@ -9,14 +9,12 @@ const MainNav = () => {
   // State variable used to manage Resources dropdown
   const [showdropDown, setShowdropDown] = useState(false);
   // state use for managing active color link
-  const [activeLink, setActiveLink] = useState('/home')
-
+  const [activeLink, setActiveLink] = useState("/home");
 
   // function for managing active color for link
   const handleLinkClick = (link) => {
     setActiveLink(link);
-    
-  }
+  };
 
   return (
     <>
@@ -36,12 +34,20 @@ const MainNav = () => {
               <Nav className="ms-auto ">
                 <Nav.Link
                   href="#home"
-                  className={`nav-link ms-3 ${activeLink === '/home' ? 'active-link' : ''}`}
-                  onClick={()=>handleLinkClick('/home')}
+                  className={`nav-link ms-3 ${
+                    activeLink === "/home" ? "active-link" : ""
+                  }`}
+                  onClick={() => handleLinkClick("/home")}
                 >
                   HOME
                 </Nav.Link>
-                <Nav.Link href="#about" classsName="">
+                <Nav.Link
+                  href="#about"
+                  className={`nav-link ms-3 ${
+                    activeLink === "/about" ? "active-link" : ""
+                  }`}
+                  onClick={() => handleLinkClick("/about")}
+                >
                   ABOUT
                 </Nav.Link>
                 <NavDropdown
@@ -51,16 +57,58 @@ const MainNav = () => {
                   onMouseEnter={() => setShowdropDown(true)}
                   onMouseLeave={() => setShowdropDown(false)}
                 >
-                  <NavDropdown.Item>Book</NavDropdown.Item>
-                  <NavDropdown.Item>Blog</NavDropdown.Item>
-                  <NavDropdown.Item>
+                  <NavDropdown.Item
+                    className={`nav-link
+                      activeLink === "/resources/book" ? "active-link" : ""
+                    }`}
+                    onClick={() => handleLinkClick("/resources/book")}
+                  >
+                    Book
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    className={`nav-link
+                      activeLink === "/resources/blog" ? "active-link" : ""
+                    }`}
+                    onClick={() => handleLinkClick("/resources/blog")}
+                  >
+                    Blog
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Item 
+                     className={`nav-link
+                      activeLink === "/resources/masteryClass" ? "active-link" : ""
+                    }`}
+                    onClick={() => handleLinkClick("/resources/masteryClass")}
+                  >
                     Purpose Mastery Masterclass
                   </NavDropdown.Item>
-                  <NavDropdown.Item>One-on-One Coaching</NavDropdown.Item>
-                  <NavDropdown.Item> Speaking Engagement </NavDropdown.Item>
+                  <NavDropdown.Item
+                     className={ `nav-link
+                      activeLink === "/resources/oneCoaching" ? "active-link" : ""
+                    }`}
+                    onClick={() => handleLinkClick("/resources/oneCoaching")}
+                  >One-on-One Coaching</NavDropdown.Item>
+                  <NavDropdown.Item
+                     className={`nav-link
+                      activeLink === "/resources/speakingEng" ? "active-link" : ""
+                    }`}
+                    onClick={() => handleLinkClick("/resources/speakingEng")}
+                  > Speaking Engagement </NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link href="#services">SHOP</Nav.Link>
-                <Nav.Link href="#services">CONTACT</Nav.Link>
+                <Nav.Link
+                  href="#services"
+                  className={`nav-link ${
+                    activeLink === "/shop" ? "active-link" : ""
+                  }`}
+                  onClick={() => handleLinkClick("/shop")}
+                >
+                  SHOP
+                </Nav.Link>
+
+                <Nav.Link href="#services"
+                  className={`nav-link ${activeLink === '/contact' ? 'active-link' : ''}`}
+                  onClick={() => handleLinkClick('/contact')}
+                >CONTACT</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
@@ -69,7 +117,6 @@ const MainNav = () => {
         {/* landinpic section */}
         <LandingPics></LandingPics>
 
-     
         {/* Scrollable content */}
         {/* <div style={{ backgroundColor: '#fff', padding: '20px', height: '150vh' }}>
           <h1>Your Scrollable Component</h1>
