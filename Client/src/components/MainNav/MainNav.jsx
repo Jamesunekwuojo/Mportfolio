aba788a506abe2488b99b98f44aeb5bb9da2fa8b
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-09-30 17:11:51 +0100

    worked on nav items, hover effects added

--- Client/src/components/MainNav/MainNav.jsx
+++ Client/src/components/MainNav/MainNav.jsx
@@ -1,13 +1,15 @@
-
+import { useState } from 'react';
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import "./MainNav.css"
 
 const MainNav = () => {
+
+    // State variable used to manage Resources dropdown
+    const [showdropDown, setShowdropDown] = useState(false)
   return (
     <>
     
 
-      {/* Background for the next component */}
       <div style={{ backgroundColor: '#0a3d62', height: '100vh', width: '100%' }}>
         
       <Navbar fixed ="top" expand="lg" style={{ width: '80%', margin: '0 auto', backgroundColor: '#0a3d62',  } } className='p-4'>
@@ -16,11 +18,14 @@ const MainNav = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto ">
-              <Nav.Link href="#home" classsName='ms-3'>HOME</Nav.Link>
-              <Nav.Link href="#about" classsName='ms-3'>ABOUT</Nav.Link>
-              <NavDropdown title={"RESOURCES"}>
+              <Nav.Link href="#home" classsName=''>HOME</Nav.Link>
+              <Nav.Link href="#about" classsName=''>ABOUT</Nav.Link>
+              <NavDropdown className='navDropdown' title={"RESOURCES"} show={showdropDown} onMouseEnter={()=> setShowdropDown(true)} onMouseLeave={() => setShowdropDown(false)}>
                 <NavDropdown.Item>Book</NavDropdown.Item>
-                <NavDropdown.Item>Book</NavDropdown.Item>
+                <NavDropdown.Item>Blog</NavDropdown.Item>
+                <NavDropdown.Item>Purpose Mastery Masterclass</NavDropdown.Item>
+                <NavDropdown.Item>One-on-One Coaching</NavDropdown.Item>
+                <NavDropdown.Item> Speaking Engagement </NavDropdown.Item>
               </NavDropdown>
               <Nav.Link href="#services" >SHOP</Nav.Link>
               <Nav.Link href="#services">CONTACT</Nav.Link>
