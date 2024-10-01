124150dc647828a9cc8861125944a715273845b9
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-10-01 23:48:04 +0100

    About us section created

---
+++ Client/src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const About = () => {
+    <Container>
+        <Row className="w-80, border rounded ">
+            <Col lg={16}>
+            </Col>
+            <Col lg={6}>
+            <p></p>
+            </Col>
+        </Row>
+    </Container>
+
+}
+
+export default About;
