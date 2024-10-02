690a7daa725a34e038b67dd37b1d1977dd094a35
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-10-02 12:41:42 +0100

    call to action component added

---
+++ Client/src/components/CalltoAction/CalltoAction.jsx
@@ -0,0 +1,25 @@
+import {Container, Row} from 'react-bootstrap';
+
+const CalltoAction = () => {
+
+    return (
+        <Container>
+            <Row>
+                <div>
+                    <img src='image3.jpg' alt='abiodun image'/>
+                </div>
+                <div>
+                    <img src="speak.jpg" alt="abiodun image" />
+                </div>
+                <div>
+                    <img src='' alt='abiodun image'/>
+                </div>
+            </Row>
+
+        </Container>
+
+    );
+
+}
+
+export default CalltoAction
--- Client/src/pages/Homepage.jsx
+++ Client/src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import About from "../components/About/About.jsx";
 import Highlight from "../components/Highlight/Highlight.jsx";
-import Featured from "../components/Featured/Featured.jsx"
+import Featured from "../components/Featured/Featured.jsx";
+import CalltoAction from "../components/CalltoAction/CalltoAction.jsx";
 import {Row} from "react-bootstrap";
 import  "./Homepage.css"
 
@@ -17,6 +18,15 @@ const Homepage = () => {
            <Row ><h2 className='subheader text-center '>Highlight</h2></Row>
 
            <Highlight></Highlight>
+
+           <Row className='parentContainer'>
+             <div>
+               <h2 className='subheader text-center bookNow'>Book Now!</h2>
+
+             </div>
+            
+            </Row>
+            <CalltoAction></CalltoAction>
 
            <Row ><h2 className='subheader text-center'>Featured</h2></Row>
            <Featured></Featured>
