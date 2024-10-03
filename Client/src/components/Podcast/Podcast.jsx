e5fbd6e70929775de6eae488f8804d5243ea6aef
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-10-03 13:00:04 +0100

    video modal conflicting issue resolved, conditionals used to render video

--- Client/src/components/Podcast/Podcast.jsx
+++ Client/src/components/Podcast/Podcast.jsx
@@ -1,39 +1,37 @@
-import {useState} from "react";
+import { useState } from "react";
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import Modalvideo1 from '../Modalvideo1/Modalvideo1';
 import Modalvideo2 from "../Modalvideo2/Modalvideo2";
-import Modal3video from "../Modal3video/Modal3video"
+import Modal3video from "../Modal3video/Modal3video";
+
 function Podcast() {
+  const [show, setShow] = useState(false); // Modal state
+  const [videoSrc, setVideoSrc] = useState('');
 
-    const [show, setShow] = useState(false); // Modal state
-    const [videoSrc, setVideoSrc] = useState({
-      vid1Src : '',
-      vid2Src : '',
-      vid3Src :'',
-    });
-  
-    const handleShow = () => {
-      setVideoSrc({
-        // vidSrc1 :  "https://www.youtube.com/embed/1vjZXQgrwo8?si=X_4NqEi4Yd_XQosz",
-        
+  const handleShow = (videoKey) => {
+    let src = '';
+    switch (videoKey) {
+      case 1:
+        src = "https://www.youtube.com/embed/1vjZXQgrwo8?si=X_4NqEi4Yd_XQosz";
+        break;
+      case 2:
+        src = "https://www.youtube.com/embed/4lbvpZaA2WM?si=HhX_Yxe2QHWuIXXx";
+        break;
+      case 3:
+        src = "https://www.youtube.com/embed/KOmNT-VuAsk?si=i5EsntFEKgOEtZL2";
+        break;
+      default:
+        break;
+    }
 
-        vid1Src: "https://www.youtube.com/embed/4lbvpZaA2WM?si=HhX_Yxe2QHWuIXXx",
+    setVideoSrc(src); // To Set the appropriate video source
+    setShow(true); // To  Show modal
+  };
 
-        vid2Src: "https://www.youtube.com/embed/4lbvpZaA2WM?si=HhX_Yxe2QHWuIXXx",
-        vid3Src: "https://www.youtube.com/embed/KOmNT-VuAsk?si=i5EsntFEKgOEtZL2"
-      }
-
-      
-
-       
-      ); // Set video source
-      setShow(true); // Show modal
-    };
-  
-    const handleClose = () => {
-      setShow(false); // Hide modal
-      setVideoSrc(""); // Reset video source
-    };
+  const handleClose = () => {
+    setShow(false); //  To Hide modal
+    setVideoSrc(""); // To  Reset video source
+  };
 
   return (
     <Container className='mt-4'>
@@ -50,8 +48,7 @@ function Podcast() {
               title="Video 1"
             ></iframe>
             <div className="d-flex justify-content-between mt-2">
-              <Button variant="link" data-bs-toggle="modal"
-              onClick={handleShow} data-bs-target="#videoModal1">
+              <Button variant="link" onClick={() => handleShow(1)}>
                 <i className="bi bi-eye"></i> Watch Video
               </Button>
               <Button variant="link" href="https://www.youtube.com/watch?v=1vjZXQgrwo8&t" target="_blank">
@@ -73,8 +70,7 @@ function Podcast() {
               title="Video 2"
             ></iframe>
             <div className="d-flex justify-content-between mt-2">
-              <Button variant="link" data-bs-toggle="modal"
-               onClick={handleShow}  data-bs-target="#videoModal2">
+              <Button variant="link" onClick={() => handleShow(2)}>
                 <i className="bi bi-eye"></i> Watch Video
               </Button>
               <Button variant="link" href="https://www.youtube.com/watch?v=4lbvpZaA2WM" target="_blank">
@@ -96,7 +92,7 @@ function Podcast() {
               title="Video 3"
             ></iframe>
             <div className="d-flex justify-content-between mt-2">
-              <Button variant="link" data-bs-toggle="modal" data-bs-target="#videoModal3" onClick={handleShow}>
+              <Button variant="link" onClick={() => handleShow(3)}>
                 <i className="bi bi-eye"></i> Watch Video
               </Button>
               <Button variant="link" href="https://www.youtube.com/watch?v=KOmNT-VuAsk" target="_blank">
@@ -105,12 +101,9 @@ function Podcast() {
             </div>
 
             {/* video modals */}
-            <Modalvideo1 show={show} onHide={handleClose} videoSrc={videoSrc.vid1Src}></Modalvideo1>
-
-            <Modalvideo2 show={show} onHide={handleClose} videoSrc={videoSrc.vid2Src}></Modalvideo2>
-
-            <Modal3video show={show} onHide={handleClose} videoSrc={videoSrc.vid3Src}></Modal3video>
-
+            <Modalvideo1 show={show && videoSrc === "https://www.youtube.com/embed/1vjZXQgrwo8?si=X_4NqEi4Yd_XQosz"} onHide={handleClose} videoSrc={videoSrc}></Modalvideo1>
+            <Modalvideo2 show={show && videoSrc === "https://www.youtube.com/embed/4lbvpZaA2WM?si=HhX_Yxe2QHWuIXXx"} onHide={handleClose} videoSrc={videoSrc}></Modalvideo2>
+            <Modal3video show={show && videoSrc === "https://www.youtube.com/embed/KOmNT-VuAsk?si=i5EsntFEKgOEtZL2"} onHide={handleClose} videoSrc={videoSrc}></Modal3video>
           </div>
         </Col>
       </Row>
