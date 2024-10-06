21bfe37bfb9f38634ceacf2b8bdf0693662f8653
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-10-06 23:21:28 +0100

    updating fonts

--- Client/src/components/MainAbout/MainAbout.jsx
+++ Client/src/components/MainAbout/MainAbout.jsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col, Image } from "react-bootstrap";
+import "./MainAbout.css"
 
 const MainAbout = () => {
   return (
@@ -13,22 +14,22 @@ const MainAbout = () => {
               </div>
               <h3 className="lh-base mb-0">ABIODUN MUSTAPHA</h3>
             </div>
-            <p className="mb-4">
+            <p id="text1" className="mb-4">
               Abiodun Mustapha is a Personal Development Expert, Business
               Development Consultant, and Founder of Growth Hub Africa, a
               community of strategic career leaders and business experts.
             </p>
-            <p className="mb-4">
+            <p id="text2" className="mb-4">
               He helps professionals and businesses through a process of
               purpose, productivity, and profitability. This he does through
               speaking engagements, training, personal coaching, and writing.
             </p>
-            <p className="mb-4">
+            <p id="text3" className="mb-4">
               He is a Certified Business Trainer and Coach by the German
               Development Corporation (GIZ) - 2018 and has trained over 2,000
               and coached over 45 professionals and business owners.
             </p>
-            <p className="mb-4">
+            <p id="text4" className="mb-4">
               He has been featured on various platforms such as the Young
               Professional Forum (Ketu, Oshodi, and Isheri-Idimu), The Prophetic
               Woman, Dear Potential, Sweet FM, Edufrica, Prevarsity Conference,
@@ -52,7 +53,7 @@ const MainAbout = () => {
                 />
               </Col>
             </Row>
-            <div className="d-flex align-items-center mb-3">
+            <div id="textAbout" className="d-flex align-items-center mb-3">
               <p className="mb-4">
                 He was selected to be one of the selected members of the Future
                 of Computing Academy by the Association of Computing Machinery,
@@ -61,13 +62,13 @@ const MainAbout = () => {
                 over the world.
               </p>
             </div>
-            <p className="mb-4">
+            <p id="text5" className="mb-4">
               He is the Convener of a city-to-city event, Purpose Hangout which
               has impacted over 500 young people from 6 cities in Nigeria. This
               has led to positioning them correctly for their life’s purposes.
             </p>
             <div className="d-flex align-items-center mb-3">
-              <p className="mb-4">
+              <p id="text6" className="mb-4">
                 Abiodun has been inspired to write thought-provoking articles
                 and books which have caused remarkable transformation of
                 mindset.
