15fdf1a21e7c80ff588a2da9a39f15b2ffac8ba8
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-10-06 01:13:18 +0100

    Contact component updated

--- Client/src/components/Contact/Contact.jsx
+++ Client/src/components/Contact/Contact.jsx
@@ -1,29 +1,92 @@
-import {Container, Row, Col, Form} from "react-bootstrap";
-
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
 
 const Contact = () => {
-    return ( 
+  return (
+    <Container className="mt-4">
+      <Row>
+        <Col>
+        <div className="sideDiv">
+            <p>My Office:</p>
+            <h3>Lagos Nigeria</h3>
+            <hr></hr>
 
-        <Container>
-            <Row>
-                <Col lg={6}>
+            <p>Mobile No:</p>
+            <h3>+234-814-856-0609
+            </h3>
+            <hr></hr>
 
-                </Col>
-                <Col>
-                <Form>
-                    <Form.Group>
-                        <Form.Label>Name:</Form.Label>
-                        <Form.Control placeholder="Enter name"></Form.Control>
 
-                        <Form.Label>Email:</Form.Label>
-                        <Form.Control placeholder="Enter email addrresses"></Form.Control>
-                    </Form.Group>
-                </Form>
-                </Col>
+            <p>Email:</p>
+            <h3>contact@abiodunmustapha.com</h3>
+            <hr></hr>
+
+            <p>Connect with me on:</p>
+            <h3>Lagos Nigeria</h3>
+            <hr></hr>
+
+            
+        </div>
+        </Col>
+        <Col className="mt-4">
+          <Form
+          >
+            <Row className="g-3">
+              <Col md={6}>
+                <Form.Floating>
+                  <Form.Control
+                    name="name"
+                    type="text"
+                    id="name"
+                    placeholder="Your Name"
+                  />
+                  <Form.Label>Name:</Form.Label>
+                </Form.Floating>
+              </Col>
+              <Col md={6}>
+                <Form.Floating>
+                  <Form.Control
+                    name="email"
+                    type="email"
+                    id="email"
+                    placeholder="Your Email"
+                  />
+                 <Form.Label>Email:</Form.Label>
+                </Form.Floating>
+              </Col>
+              <Col xs={12}>
+                <Form.Floating>
+                  <Form.Control
+                    name="subject"
+                    type="text"
+                    id="subject"
+                    placeholder="Subject"
+                  />
+                 <Form.Label>Subject:</Form.Label>
+                </Form.Floating>
+              </Col>
+              <Col xs={12}>
+                <Form.Floating>
+                  <Form.Control
+                    as="textarea"
+                    name="message"
+                    id="message"
+                    placeholder="Leave a message here"
+                    style={{ height: "100px" }}
+                  />
+                  <Form.Label >Message</Form.Label>
+                </Form.Floating>
+              </Col>
+              <Col xs={12}>
+                <Button className="btn btn-primary py-3 px-5" type="submit">
+                  Send Message
+                </Button>
+              </Col>
             </Row>
-        </Container>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-    );
-}
-
-export default Contact; 
+export default Contact;
