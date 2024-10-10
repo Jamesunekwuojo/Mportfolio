d5bc6e3360f7ad51d31134cdbc995ab825d02e89
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-10-10 03:17:09 +0100

    overlow issue finally resolved

--- Client/src/components/VideoSection.jsx
+++ Client/src/components/VideoSection.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
-import "./VideoSection.css"
+import "./VideoSection.css";
 
 const VideoSection = () => {
   const [show, setShow] = useState(false); // Modal state
@@ -21,12 +21,18 @@ const VideoSection = () => {
   return (
     <div>
       {/* Button to trigger modal */}
-      {/* <Button className=" button mx-4" onClick={handleShow}> */}
-        {/* <span> */}
-        <svg className="videoSvg" onClick = {handleShow} fill="#ff9900" width="80px" height="80px" viewBox="0 -8 528 528" xmlns="http://www.w3.org/2000/svg" ><title>video</title><path d="M264 456Q211 456 164 429 118 402 91 356 64 310 64 256 64 202 91 156 118 110 164 83 210 56 264 56 318 56 364 83 410 110 437 156 464 202 464 256 464 309 437 356 410 402 364 429 318 456 264 456ZM345 256L216 160 216 352 345 256Z" /></svg>
-        {/* </span> */}
-      {/* </Button> */}
-      {/* <h5 className="ms-4 mb-0 d-none d-sm-block">Watch Video</h5> */}
+      <svg
+        className="videoSvg"
+        onClick={handleShow}
+        fill="#ff9900"
+        width="80px"
+        height="80px"
+        viewBox="0 -8 528 528"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title>video</title>
+        <path d="M264 456Q211 456 164 429 118 402 91 356 64 310 64 256 64 202 91 156 118 110 164 83 210 56 264 56 318 56 364 83 410 110 437 156 464 202 464 256 464 309 437 356 410 402 364 429 318 456 264 456ZM345 256L216 160 216 352 345 256Z" />
+      </svg>
 
       {/* Modal for playing video */}
       <Modal show={show} onHide={handleClose} size="lg" centered>
@@ -36,6 +42,7 @@ const VideoSection = () => {
             <a
               href="https://www.youtube.com/@AbiodunMustapha/featured"
               target="_blank"
+              rel="noopener noreferrer"
             >
               {" "}
               Youtube Channel
@@ -50,18 +57,13 @@ const VideoSection = () => {
               src={videoSrc}
               title="Video Player"
               width="100%"
-              height="400"
+              height="100%"
               style={{ border: "none" }}
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
           </div>
         </Modal.Body>
-        {/* <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-            </Modal.Footer> */}
       </Modal>
     </div>
   );
